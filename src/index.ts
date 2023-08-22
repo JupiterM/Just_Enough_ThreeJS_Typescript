3e0169e114097a4418aba1b9ed01bfc04bbec622
jupes <matteoharmon@gmail.com>
2023-08-21 19:26:13 -0500

    Added Demo Scene for Display.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import * as THREE from 'three';
 
+//Set up Scene, Renderer and Camera
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+document.body.appendChild( renderer.domElement );
 
+//Create 3D Object to be displayed
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-console.log("Hello Typescript!");
+camera.position.z = 5;
 
-const canvas = document.body.appendChild(document.createElement("canvas"));
-
-const welcome = document.body.appendChild(document.createElement("div"));
-welcome.innerHTML = "<h2>Welcome to the App</h2>";
+//Animate Function
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
